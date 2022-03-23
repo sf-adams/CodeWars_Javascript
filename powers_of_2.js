3c87e5ab4bedd7153fba07d0798cd975395e9be1
Sam <samuel.fitz@btinternet.com>
2022-03-23 11:47:21 +0000

    Power of 2 - Comments Added

--- powers_of_2.js
+++ powers_of_2.js
@@ -10,11 +10,26 @@ n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
 */
 
 function powersOfTwo(n) {
+
+  // Create new array for storing the powers
   let result = [];
+
+  // Create a for loop, starting at index position 0, setting
+  // an upper limit of n, and incrementing by 1
   for (var i = 0; i <= n; i++) {
-    result.push(Math.pow(2, i));
+
+    // Use the push method to add each new element to the end of the array
+    result.push(2 ** i);
+
+    // An alternative that uses the Math.pow() static method.
+    // Give it two arguments, base and exponent, and it returns
+    // base to the power of exponent:
+    // result.push(Math.pow(2, i));
   }
+
+  //
   return result;
 }
 
+// Print the result of the function with an example of n = 6
 console.log(powersOfTwo(6));
