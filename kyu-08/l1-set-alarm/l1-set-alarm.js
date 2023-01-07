bba46f8112922fd5f1e1f9b39c52264331653608
Sam <samuel.fitz@btinternet.com>
2023-01-07 12:21:48 +0000

    feat(8 kyu): complete l1-set-alarm

---
+++ kyu-08/l1-set-alarm/l1-set-alarm.js
@@ -0,0 +1,13 @@
+function setAlarm(employed, vacation) {
+  if (employed == true && vacation == true) {
+    return false;
+  } else if (employed == false && vacation == true) {
+    return false;
+  } else if (employed == false && vacation == false) {
+    return false;
+  } else if (employed == true && vacation == false) {
+    return true;
+  }
+}
+
+module.exports = setAlarm;
---
+++ kyu-08/l1-set-alarm/l1-set-alarm.test.js
@@ -0,0 +1,16 @@
+const setAlarm = require("./l1-set-alarm");
+
+describe("Set Alarm", () => {
+  it("Should not set alarm for both holiday and employment", () => {
+    expect(setAlarm(true, true)).toBe(false);
+  });
+  it("Should not set alarm for unemployment and holiday", () => {
+    expect(setAlarm(false, true)).toBe(false);
+  });
+  it("Should not set alarm for unemployment and no holiday", () => {
+    expect(setAlarm(false, true)).toBe(false);
+  });
+  it("Should set alarm with employment and holiday", () => {
+    expect(setAlarm(false, true)).toBe(false);
+  });
+});
