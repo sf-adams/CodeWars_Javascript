12d8d9eec5787cca95cbc8b52390a1d40422aaf0
Sam <samuel.fitz@btinternet.com>
2023-01-07 12:45:28 +0000

    refactor(8kyu): l1 set alarm

--- kyu-08/l1-set-alarm/l1-set-alarm.js
+++ kyu-08/l1-set-alarm/l1-set-alarm.js
@@ -1,13 +1,23 @@
+// Original Solution
+
+// function setAlarm(employed, vacation) {
+//   if (employed == true && vacation == true) {
+//     return false;
+//   } else if (employed == false && vacation == true) {
+//     return false;
+//   } else if (employed == false && vacation == false) {
+//     return false;
+//   } else if (employed == true && vacation == false) {
+//     return true;
+//   }
+// }
+
+// module.exports = setAlarm;
+
+// My solution with refactor
+// Using multiple ternary operators (conditional chains)
 function setAlarm(employed, vacation) {
-  if (employed == true && vacation == true) {
-    return false;
-  } else if (employed == false && vacation == true) {
-    return false;
-  } else if (employed == false && vacation == false) {
-    return false;
-  } else if (employed == true && vacation == false) {
-    return true;
-  }
+  return employed == false ? false : vacation == false ? true : false;
 }
 
 module.exports = setAlarm;
