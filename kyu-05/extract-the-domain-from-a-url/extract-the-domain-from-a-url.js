dae5a1d41a656301990b58914f7dac9c4430d78c
Sam <samuel.fitz@btinternet.com>
2023-01-09 15:19:23 +0000

    refactor(5kyu): extract the domain from a url

--- kyu-05/extract-the-domain-from-a-url/extract-the-domain-from-a-url.js
+++ kyu-05/extract-the-domain-from-a-url/extract-the-domain-from-a-url.js
@@ -1,7 +1,15 @@
+// function domainName(url) {
+//   url = url.replace("http://", "");
+//   url = url.replace("https://", "");
+//   url = url.replace("www.", "");
+
+//   return url.split(".")[0];
+// }
+
+// Refactored solution
+
 function domainName(url) {
-  url = url.replace("http://", "");
-  url = url.replace("https://", "");
-  url = url.replace("www.", "");
+  url = url.replace("http://", "").replace("https://", "").replace("www.", "");
 
   return url.split(".")[0];
 }
