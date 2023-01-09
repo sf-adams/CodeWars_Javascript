e86cafd4649c9315f1fcef335ef526acc58e1583
Sam <samuel.fitz@btinternet.com>
2023-01-09 13:34:38 +0000

    perf(6kyu): unique in order

--- kyu-06/unique-in-order/unique-in-order.js
+++ kyu-06/unique-in-order/unique-in-order.js
@@ -10,3 +10,33 @@ function uniqueInOrder(iterable) {
 }
 
 module.exports = uniqueInOrder;
+
+/*
+
+Clever Solution
+- Uses spread operator and filter to return a new array without repeating
+
+
+var uniqueInOrder=function(iterable){
+    return [...iterable].filter((a, i) => a !== iterable[i-1])
+}
+
+---
+
+Best Practice Solution
+- Not disimilar to my solution
+
+function uniqueInOrder(it) {
+  var result = []
+  var last
+
+  for (var i = 0; i < it.length; i++) {
+    if (it[i] !== last) {
+      result.push(last = it[i])
+    }
+  }
+
+  return result
+}
+
+*/
